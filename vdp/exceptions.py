5b4c0f6d720cd4c00c587614a22b176515b8c6e8
muraliadithya <adithyamuralism@gmail.com>
2020-10-11 01:38:59 -0500

    Adding more meaningful exception classes

--- vdp/exceptions.py
+++ vdp/exceptions.py
@@ -4,3 +4,19 @@ class PartialInterpretationException(Exception):
     does not happen is to 'close' model interpretations with default values for each function (false for predicates).
     """
     pass
+
+
+class MalformedPuzzleException(Exception):
+    """
+    This exception is raised when a VDPPuzzle object does not fit the assumptions made by a solver.
+    Example: empty set of training/candidate images.
+    """
+    pass
+
+
+class NonsenseSolverConfigurationException(Exception):
+    """
+    This exception is raised when a solver's configuration does not make sense with respect to its assumptions.
+    Example: negative number of quantified variables.
+    """
+    pass

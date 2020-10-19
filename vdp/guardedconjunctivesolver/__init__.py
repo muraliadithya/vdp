51ec9d646c24a3d570fb979c8787544edd066990
muraliadithya <adithyamuralism@gmail.com>
2020-10-19 11:02:58 -0500

    Copying code over for building guarded conjunctive solver

--- vdp/guardedconjunctivesolver/__init__.py
+++ vdp/guardedconjunctivesolver/__init__.py
@@ -1,5 +1,4 @@
-# Package for a basic conjunctive solver
-# The intention is to model other 'useful' solvers based on the structure of this one.
+# Package for a basic conjunctive solver with unary guards.
 
 # The solver module contains the main class defining the solver call and special configuration options.
 # The formula module contains a class that defines a 'representation' in SMT of a generic formula belonging

bf16d5005cdffe19837ef6b6f6a01ab150fbb2bc
muraliadithya <adithyamuralism@gmail.com>
2021-09-30 23:53:09 -0500

    Updating guardedconjunctivealtpuzzlesolver package to use latest changes to guardedconjunctivesolver

--- vdp/guardedconjunctivealtpuzzlesolver/guardedconjunctivealtpuzzlesolver.py
+++ vdp/guardedconjunctivealtpuzzlesolver/guardedconjunctivealtpuzzlesolver.py
@@ -1,5 +1,6 @@
 from z3 import *
 
+import vdp.exceptions
 from vdp.guardedconjunctivesolver.guardedconjunctivesolver import GuardedConjunctiveSolver
 from vdp.guardedconjunctivesolver.formula import Formula
 
@@ -14,21 +15,16 @@ class GuardedConjunctiveAltpuzzleSolver(GuardedConjunctiveSolver):
 
     def solve(self, vdppuzzle):
         """
-        This represents the core functionality of the solver class.
-        The VDPPuzzle object provided in the argument is automatically transmuted into a puzzle of another kind, 
-        the specification for which is provided through an options attribute of the solver.
+        This represents the core functionality of the solver class. It takes a VDPPuzzle object and finds a
+        discriminator in the hypothesis class represented by the solver.
         """
-        # Throws exception if puzzle cannot be normalised. Returns the normalised puzzle otherwise.
-        # Disabling normalisation in favour of shifting the task to the ir package
-        # vdppuzzle = _normalise_puzzle(vdppuzzle)
 
         # Call auxiliary function to extract relations, guards, etc for the guarded fragment
         quantified_sort, forelations, guard_forelations = self._process_vocabulary(vdppuzzle)
         # Initialise the formula representation class.
         sol = Solver()
         discriminator = Formula()
-        repr_constraints = discriminator.initialise(self.num_quantified_vars, quantified_sort, forelations,
-                                                    guard_forelations, self.options)
+        repr_constraints = discriminator.initialise(self.num_quantified_vars, quantified_sort, forelations, guard_forelations, self.options)
         sol.add(repr_constraints)
 
         # Add the SMT constraints for each model.
@@ -38,7 +34,7 @@ class GuardedConjunctiveAltpuzzleSolver(GuardedConjunctiveSolver):
         # candidate_models = vdppuzzle.get_candidate_models()
         # candidate_model_constraints = [discriminator.satisfaction_constraint(model) for model in candidate_models]
         indicator_vars = None
-        puzzle_type = self.options['puzzle_type']
+        puzzle_type = self.options.get('puzzle_type', 'oddone') # Needs to be changed to have no default once the solve_puzzle module can set this option
         if puzzle_type == 'oddone':
             # Pick the odd one out from among the training models
             # Find a formula that fits all models except for one
@@ -54,19 +50,21 @@ class GuardedConjunctiveAltpuzzleSolver(GuardedConjunctiveSolver):
                 pos_constraint, neg_constraint = training_model_constraints[i]
                 # If the indicator variable is false then the models satisfies the formula, otherwise falsifies it
                 sol.add(If(indicator_vars[i], neg_constraint, pos_constraint))
+        else:
+            vdp.exceptions.NonsenseSolverConfigurationError(f'Unsupported puzzle type {puzzle_type}')
 
         # Ask for discrimination for as many times as required.
         num_discriminators = self.options.get('num_discriminators', 1)
         previous_solution_constraints = []
         sol_number = 0
+        list_of_solutions = []
         while sol_number < num_discriminators:
             # Add constraints about not giving any previous solutions
             # TODO (medium-low): Earlier constraints already present. Only add the last one.
             sol.add(previous_solution_constraints)
             soluble = sol.check()
             if soluble == z3.unsat:
-                print('No discriminator found for puzzle {}.'.format(vdppuzzle.puzzle_name))
-                exit(0)
+                return list_of_solutions
                 # raise vdpexceptions.MalformedPuzzleException("The given puzzle could not be solved by this solver.")
             smt_model = sol.model()
             # Add a constraint negating the given solution for the next round
@@ -76,7 +74,6 @@ class GuardedConjunctiveAltpuzzleSolver(GuardedConjunctiveSolver):
             candidate = next((indicator_var for indicator_var in indicator_vars
                               if smt_model.eval(indicator_var, model_completion=True)), None)
             candidate_number = int(candidate.sexpr()[1])
-            print('Candidate: {}\nConcept: {}\nCandidate Name: {}\n'.format(candidate.sexpr(), discriminant,
-                                                                            training_models[
-                                                                                candidate_number].model_name))
+            list_of_solutions.append((candidate.sexpr(), discriminant, training_models[candidate_number].model_name))
             sol_number = sol_number + 1
+        return list_of_solutions
--- vdp/guardedconjunctivealtpuzzlesolver/solve_puzzle.py
+++ vdp/guardedconjunctivealtpuzzlesolver/solve_puzzle.py
@@ -9,32 +9,9 @@ Refer the package description to learn more about the role of this module.
 """
 
 from vdp.guardedconjunctivealtpuzzlesolver.guardedconjunctivealtpuzzlesolver import GuardedConjunctiveAltpuzzleSolver
+from vdp.guardedconjunctivesolver.solve_puzzle import _solve_puzzle_aux
 
 
 def solve_puzzle(vdp_puzzle, solver_args):
-    # Extract args from solver_args parameter and configure solver appropriately
-    num_vars = solver_args.num_vars
-    solver = GuardedConjunctiveAltpuzzleSolver(num_vars=num_vars)
-    solver.options = dict()
-    # Type of puzzle
-    solver.options['puzzle_type'] = solver_args.puzzle_type
-    # Number of conjuncts in the matrix
-    num_conjuncts_bound = solver_args.num_conjuncts
-    if num_conjuncts_bound is not None:
-        solver.options['num_conjuncts_bound'] = num_conjuncts_bound
-    # Quantifier shape
-    if solver_args.existential_only:
-        solver.options['quantifier_shape'] = 'e' * num_vars
-    elif solver_args.universal_only:
-        solver.options['quantifier_shape'] = 'a' * num_vars
-    elif solver_args.quantifier_shape is not None:
-        solver.options['quantifier_shape'] = solver_args.quantifier_shape
-    # Vcauity
-    no_vacuity = solver_args.no_vacuity
-    if no_vacuity:
-        solver.options['no_vacuity'] = True
-    # Number of discriminators
-    solver.options['num_discriminators'] = solver_args.num_discriminators
-
-    # Solve puzzle
-    solver.solve(vdp_puzzle)
+    SolverClass = GuardedConjunctiveAltpuzzleSolver
+    _solve_puzzle_aux(vdp_puzzle, SolverClass, solver_args)
